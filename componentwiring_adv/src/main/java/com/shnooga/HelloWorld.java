f43a974594c3a07825b1f99d35db853baa46aa53
Huey Nguyen <HueyNg@vsp.com>
2017-01-19 01:19:38 +0000

    Code clean up

--- componentwiring_adv/src/main/java/com/shnooga/HelloWorld.java
+++ componentwiring_adv/src/main/java/com/shnooga/HelloWorld.java
@@ -1,6 +1,5 @@
 package com.shnooga;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // Use @Component to avoid defining the bean in SpringBeans.xml

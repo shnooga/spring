6d08f8ccf20b2621fa8d9d2fd43bd1a109503513
Jane Saint <papersnail@gmail.com>
2017-09-08 22:01:44 +0100

    More Comments

--- practice/src/main/java/com/shnooga/bean/food/JellyBean.java
+++ practice/src/main/java/com/shnooga/bean/food/JellyBean.java
@@ -1,5 +1,6 @@
 package com.shnooga.bean.food;
 
+// @Component not needed because already declared in BeanIntf
 public class JellyBean implements  BeanIntf {
     public String getBehavior() { return "sweet"; }
     public String getName() { return getClass().getSimpleName(); }

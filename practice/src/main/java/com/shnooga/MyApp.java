e7843e05bf7857826070ffa484e66ed8a8a47404
Huey Nguyen <HueyNg@vsp.com>
2017-10-24 18:10:44 +0100

    Singleton example code addition

--- practice/src/main/java/com/shnooga/MyApp.java
+++ practice/src/main/java/com/shnooga/MyApp.java
@@ -1,5 +1,6 @@
 package com.shnooga;
 
+import com.shnooga.bean.Plate;
 import com.shnooga.bean.Table;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,5 +12,9 @@ public class MyApp {
         Table table = ctx.getBean(Table.class);
 
         table.showContent();
+
+        Plate rndPlate1 = (Plate) ctx.getBean("roundPlate");
+        Plate rndPlate2 = (Plate) ctx.getBean("roundPlate");
+        System.out.println("rndPlate1 and rndPlate2 are actually he same instance due to default singleton creation.");
     }
 }

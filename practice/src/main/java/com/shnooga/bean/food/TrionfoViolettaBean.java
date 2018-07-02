1be16d3bc27b6192cedeb3a06360bfed54cf1c64
Huey Nguyen <HueyNg@vsp.com>
2018-07-03 00:45:06 +0100

    Code clean up

--- practice/src/main/java/com/shnooga/bean/food/TrionfoViolettaBean.java
+++ practice/src/main/java/com/shnooga/bean/food/TrionfoViolettaBean.java
@@ -1,10 +1,8 @@
 package com.shnooga.bean.food;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
-@Qualifier("purpleBean")
+@Component("purpleBean")
 public class TrionfoViolettaBean implements  BeanIntf {
     public String getBehavior() { return "purplish"; }
     public String getName() { return getClass().getSimpleName(); }
